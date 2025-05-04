0fe12fe02c810161fb0de23c5476ac9866eb444a
ummesalma303 <ummesalma333999@gmail.com>
2025-05-04 18:13:34 +0600

    added day-1 practice

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log('hello..........')
+function updateName() {
+    const name = prompt('Enter a new name');
+    const b = btn.textContent=`Player 1: ${name}`
+    console.log(b)
+}
+const btn = document.querySelector('.button')
+btn.addEventListener("click", updateName)
