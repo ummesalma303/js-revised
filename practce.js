ce0a36e5fce6af55f4b31a1104d736c023d3c5b3
ummesalma303 <ummesalma333999@gmail.com>
2025-05-06 20:44:43 +0600

    variable and number practice done

--- practce.js
+++ practce.js
@@ -4,6 +4,9 @@ function checkGuess() {
   const sub = document.querySelector('.guessSubmit')
   sub.addEventListener('click',checkGuess)
 
+  
+  /* -------------------------------- variable -------------------------------- */
+
   const button = document.getElementById('button_A')
   const heading = document.querySelector('#heading_A')
 
@@ -12,3 +15,17 @@ function checkGuess() {
     button.textContent = 'try again'
     heading.textContent = `your score: ${count++}`
   }
+
+
+//   let myNumber = "74";
+// myNumber += 3;
+// console.log(myNumber)
+
+// let myNumber = "74";
+// myNumber = Number(myNumber) + 3;
+
+let num2 = 50
+let num1 = 10
+let myNumber = num2 + num1 / 8 + 2;
+
+console.log(myNumber)
